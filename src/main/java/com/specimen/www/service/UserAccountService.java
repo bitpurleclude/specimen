6247c40f8827e3bb5233b5e53643557e37a27e1b
bitpurleclude <1551758442@qq.com>
2024-04-13 10:55:13 +0800

    feat(新增service):

---
+++ src/main/java/com/specimen/www/service/UserAccountService.java
@@ -0,0 +1,9 @@
+package com.specimen.www.service;
+
+import com.specimen.www.bean.UserAccount;
+import org.springframework.stereotype.Service;
+
+
+public interface UserAccountService {
+    void insertUserAccount(UserAccount userAccount);
+}
