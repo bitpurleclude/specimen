8d6f8590bfb67169b383b1539438a739ff2ba513
bitpurleclude <1551758442@qq.com>
2024-04-13 14:24:43 +0800

    refactor:
    删除无用的代码userAccount类

--- src/main/java/com/specimen/www/service/UserAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.specimen.www.service;
-
-import com.specimen.www.bean.UserAccount;
-import org.springframework.stereotype.Service;
-
-
-public interface UserAccountService {
-    void insertUserAccount(UserAccount userAccount);
-}
