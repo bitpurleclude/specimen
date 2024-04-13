a60a180eb3da0baa3c445d6f4cc94bc5d4db40c8
bitpurleclude <1551758442@qq.com>
2024-04-13 14:23:08 +0800

    feat:
    继承了spring的userDetail，并加入了检测是否注册和用户插入数据库

--- src/main/java/com/specimen/www/impl/UserServiceImpl.java
+++ src/main/java/com/specimen/www/impl/UserServiceImpl.java
@@ -1,13 +1,37 @@
 package com.specimen.www.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.specimen.www.bean.User;
+import com.specimen.www.mapper.UserMapper;
 import com.specimen.www.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService , UserDetailsService {
+    @Autowired
+    UserMapper userMapper;
     @Override
     public void insertUser(User user) {
 
+    }
+   public User getUserByUsername(String username) {
+       QueryWrapper<User> query = new QueryWrapper(User.class);
+       query.eq("email",username);
+       return userMapper.selectOne(query);
+   }
+    public boolean checkHasResign(String email) {
+         return getUserByUsername(email)==null;
+    }
+    public void insertUserByEmail(User user) {
+        userMapper.insert(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
