fe34019c2e95822129a67e02f2a023e5ce78717b
bitpurleclude <1551758442@qq.com>
2024-04-13 10:51:18 +0800

    feat(impl): 新增impl
    新增impl
    
    BREAKING CHANGE: 新增impl

---
+++ src/main/java/com/specimen/www/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.specimen.www.impl;
+
+import com.specimen.www.bean.User;
+import com.specimen.www.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void insertUser(User user) {
+
+    }
+}
