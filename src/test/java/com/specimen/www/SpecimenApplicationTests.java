5faaf8aea868ee5b3048d9bb9b40f77d207a5836
bitpurleclude <1551758442@qq.com>
2024-04-14 11:18:02 +0800

    feat:
    新增用户注入测试

--- src/test/java/com/specimen/www/SpecimenApplicationTests.java
+++ src/test/java/com/specimen/www/SpecimenApplicationTests.java
@@ -2,12 +2,15 @@ package com.specimen.www;
 
 import com.specimen.www.bean.ImageWithSignPath;
 import com.specimen.www.bean.Role;
+import com.specimen.www.bean.User;
 import com.specimen.www.impl.RoleServiceImpl;
+import com.specimen.www.impl.UserServiceImpl;
 import com.specimen.www.mapper.ImageWithSignMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -16,6 +19,9 @@ import java.util.List;
 class SpecimenApplicationTests {
     @Autowired
     private ImageWithSignMapper imageWithSignMapper;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
         ImageWithSignPath a=new ImageWithSignPath(0,"a");
@@ -27,5 +33,15 @@ class SpecimenApplicationTests {
     void roleServiceTest(){
         List<Role> allRole = roleService.getAllRole();
         System.out.println(allRole);
+    }
+    @Autowired
+    UserServiceImpl userService;
+    @Test
+    void userServiceTest(){
+        User user=new User();
+        user.setEmail("test1");
+        user.setPassword(passwordEncoder.encode("123456"));
+        userService.checkHasResign("test");
+        userService.insertUser(user);
     }
 }
