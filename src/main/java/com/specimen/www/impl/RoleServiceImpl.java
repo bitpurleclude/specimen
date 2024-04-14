5394016e460007a8d817c7c522b8ef7d1236a974
bitpurleclude <1551758442@qq.com>
2024-04-14 11:17:37 +0800

    feat:
    通过bean注入role

--- src/main/java/com/specimen/www/impl/RoleServiceImpl.java
+++ src/main/java/com/specimen/www/impl/RoleServiceImpl.java
@@ -4,6 +4,7 @@ import com.specimen.www.bean.Role;
 import com.specimen.www.mapper.RoleMapper;
 import com.specimen.www.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class RoleServiceImpl implements RoleService {
     @Autowired
     RoleMapper roleMapper;
     @Override
+    @Bean
     public List<Role> getAllRole() {
         //通过空条件查询所有role
         List<Role> roles=roleMapper.selectList(null);
