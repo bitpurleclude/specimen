0bafc8374ec24c0c4db8995200ee450253f646c6
bitpurleclude <1551758442@qq.com>
2024-04-14 11:17:08 +0800

    feat:
    开启了http访问

---
+++ src/main/java/com/specimen/www/config/JavaStackApplication.java
@@ -0,0 +1,30 @@
+package com.specimen.www.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JavaStackApplication {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStackApplication.class, args);
+    }
+
+}
