311307bad73ea173e7f01d3eeb75b0bebd9fd2fc
bitpurleclude <1551758442@qq.com>
2024-04-17 18:06:42 +0800

    feat:
    新建roleId与UserId对应类serviceImpl

---
+++ src/main/java/com/specimen/www/impl/UserIdToRoleIdServiceImpl.java
@@ -0,0 +1,19 @@
+package com.specimen.www.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.specimen.www.bean.UserIdToRoleId;
+import com.specimen.www.mapper.UserIdToRoleIdMapper;
+import com.specimen.www.service.UserIdToRoleIdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserIdToRoleIdServiceImpl implements UserIdToRoleIdService {
+    @Autowired
+    private UserIdToRoleIdMapper userIdToRoleIdMapper;
+    public List<UserIdToRoleId> getUserIdToRoleIdByUserId(int userId) {
+        return userIdToRoleIdMapper.selectList(new QueryWrapper<UserIdToRoleId>().eq("user_id", userId));
+    }
+}
