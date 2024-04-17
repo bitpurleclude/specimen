223fa6a057233a3b86fabf8d5ba75d5c074040e5
bitpurleclude <1551758442@qq.com>
2024-04-17 18:06:23 +0800

    feat:
    新建roleId与UserId对应类mapper

---
+++ src/main/java/com/specimen/www/mapper/UserIdToRoleIdMapper.java
@@ -0,0 +1,7 @@
+package com.specimen.www.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.specimen.www.bean.UserIdToRoleId;
+
+public interface UserIdToRoleIdMapper extends BaseMapper<UserIdToRoleId> {
+}
