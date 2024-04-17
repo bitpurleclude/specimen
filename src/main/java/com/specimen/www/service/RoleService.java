47d50007319f4c2e5f95f4dc6ab9d5463a94fa55
bitpurleclude <1551758442@qq.com>
2024-04-17 18:00:30 +0800

    feat:
    增加roleService新接口

--- src/main/java/com/specimen/www/service/RoleService.java
+++ src/main/java/com/specimen/www/service/RoleService.java
@@ -1,9 +1,15 @@
 package com.specimen.www.service;
 
 import com.specimen.www.bean.Role;
+import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public interface RoleService {
-    List<Role> getAllRole();
+    @Bean
+    HashMap<String, Role> getAllRoleName();
+
+    @Bean
+    HashMap<Integer, Role> getAllRoleId();
 }
