80fc080eb7acb5c2260b81e4889111372e025c2d
bitpurleclude <1551758442@qq.com>
2024-04-17 18:01:59 +0800

    feat:
    增加roleService新接口

--- src/main/java/com/specimen/www/impl/RoleServiceImpl.java
+++ src/main/java/com/specimen/www/impl/RoleServiceImpl.java
@@ -7,16 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+
 @Service
 public class RoleServiceImpl implements RoleService {
     @Autowired
     RoleMapper roleMapper;
     @Override
     @Bean
-    public List<Role> getAllRole() {
+    public HashMap<String, Role> getAllRoleName() {
         //通过空条件查询所有role
-        List<Role> roles=roleMapper.selectList(null);
-        return roles;
+        List<Role> roles = roleMapper.selectList(null);
+        HashMap<String,Role> roleHashMap = new HashMap<>();
+       for (Role role : roles) {
+           roleHashMap.put(role.getRoleName(),role);
+       }
+        return roleHashMap;
+    }
+    @Override
+    @Bean
+    public HashMap<Integer, Role> getAllRoleId() {
+        //通过空条件查询所有role
+        List<Role> roles = roleMapper.selectList(null);
+        HashMap<Integer,Role> roleHashMap = new HashMap<>();
+        for (Role role : roles) {
+            roleHashMap.put(role.getId(),role);
+        }
+        return roleHashMap;
+    }
+    public void insertRoleToUser(int userId,Role role){
+
     }
 }
