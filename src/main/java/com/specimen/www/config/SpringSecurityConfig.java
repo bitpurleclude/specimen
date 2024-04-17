b405f47a6daaac5c03122e9a6769e1bb7eee14fd
bitpurleclude <1551758442@qq.com>
2024-04-17 18:04:43 +0800

    feat:
    替换自制的用户验证类，关闭csrf，设置用户登录接口

--- src/main/java/com/specimen/www/config/SpringSecurityConfig.java
+++ src/main/java/com/specimen/www/config/SpringSecurityConfig.java
@@ -1,32 +1,51 @@
 package com.specimen.www.config;
+import com.alibaba.fastjson.JSON;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 
-public class SpringSecurityConfig {
+public class SpringSecurityConfig  {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(loginFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))), UsernamePasswordAuthenticationFilter.class)
+                .csrf((csrf) -> csrf.disable())
                 .formLogin(formLogin -> formLogin
+                        .loginProcessingUrl("/user/authentication/login").permitAll()
                         .loginPage("/user/authentication/html/Login.html")
                         .permitAll()
                 )
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user/register").permitAll()
                         .requestMatchers("/user/authentication/*").permitAll()
-                        .requestMatchers("/user/*").permitAll()
                         .requestMatchers("/hello").permitAll()
                         .requestMatchers("/html/SvgDraw.html").permitAll()
                         .anyRequest().authenticated()
@@ -34,5 +53,13 @@ public class SpringSecurityConfig {
                 ;
 
         return http.build();
+    }
+    CustomUsernamePasswordAuthenticationFilter loginFilter(AuthenticationManager authenticationManager) throws Exception {
+        CustomUsernamePasswordAuthenticationFilter loginFilter = new CustomUsernamePasswordAuthenticationFilter(authenticationManager);
+        loginFilter.setFilterProcessesUrl("/user/authentication/login");
+        loginFilter.setUsernameParameter("email");
+        loginFilter.setPasswordParameter("password");
+//        loginFilter.setAuthenticationManager();
+        return loginFilter;
     }
 }
