58992e7e1e3fd1350c5d2c03fc22fe26986d6114
bitpurleclude <1551758442@qq.com>
2024-04-17 18:07:26 +0800

    feat:
    新增是否已经注册接口和通过名字获取用户

--- src/main/java/com/specimen/www/impl/UserServiceImpl.java
+++ src/main/java/com/specimen/www/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.specimen.www.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.specimen.www.bean.Role;
 import com.specimen.www.bean.User;
+import com.specimen.www.bean.UserIdToRoleId;
 import com.specimen.www.mapper.UserMapper;
+import com.specimen.www.service.UserIdToRoleIdService;
 import com.specimen.www.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,18 +14,33 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService , UserDetailsService {
     @Autowired
     UserMapper userMapper;
     @Autowired
     private PasswordEncoder passwordEncoder;
-
+    @Autowired
+    private UserIdToRoleIdServiceImpl userIdToRoleIdService;
+    @Autowired
+    private HashMap<Integer, Role> roleIdHashMap;
+    @Override
     public User getUserByUsername(String username) {
-       QueryWrapper<User> query = new QueryWrapper(User.class);
-       query.eq("email",username);
-       return userMapper.selectOne(query);
-   }
+        QueryWrapper<User> query = new QueryWrapper(User.class);
+        query.eq("email",username);
+        User user = userMapper.selectOne(query);
+        if (user!=null){
+//            List<UserIdToRoleId> userIdToRoleIdByUserId = userIdToRoleIdService.getUserIdToRoleIdByUserId(user.getId());
+//            for (UserIdToRoleId userIdToRoleId : userIdToRoleIdByUserId) {
+//                user.setRole(roleIdHashMap.get(userIdToRoleId.getRoleId()));
+//            }
+        }
+        return user;
+    }
+    @Override
     public boolean checkHasResign(String email) {
          return getUserByUsername(email)!=null;
     }
@@ -34,7 +52,7 @@ public class UserServiceImpl implements UserService , UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user=userMapper.selectOne(new QueryWrapper<User>().eq("email",username));
+        User user=getUserByUsername(username);
         if (user==null){
             try {
                 throw UsernameNotFoundException.class.newInstance();
