ef0f43b10c02f7020577debc843b7f99779a4b68
bitpurleclude <1551758442@qq.com>
2024-04-17 17:59:26 +0800

    feat:
    重写springSecurity的用户管理方法

---
+++ src/main/java/com/specimen/www/config/MysqlUserDetailsManager.java
@@ -0,0 +1,44 @@
+package com.specimen.www.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+public class MysqlUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return false;
+    }
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        return null;
+    }
+}
