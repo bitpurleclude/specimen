532976a9e48dc091d6c6fd01d094bc7bd60437b0
bitpurleclude <1551758442@qq.com>
2024-04-21 15:15:11 +0800

    feat:
    新增获取角色函数

--- src/main/java/com/specimen/www/service/UserService.java
+++ src/main/java/com/specimen/www/service/UserService.java
@@ -6,6 +6,8 @@ import com.specimen.www.bean.User;
 public interface UserService {
     User getUserByUsername(String username);
 
+    User getUserRoleByUser(User user);
+
     boolean checkHasResign(String email);
 
     void insertUser(User user);
