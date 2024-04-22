63bfad73464cb57bf04e6c257a43c4d68b0f1349
bitpurleclude <1551758442@qq.com>
2024-04-22 22:10:32 +0800

    feat:
    重新设计图片和svg路径的bean

--- src/main/java/com/specimen/www/bean/ImageWithSignPath.java
+++ src/main/java/com/specimen/www/bean/ImageWithSignPath.java
@@ -1,15 +1,30 @@
 package com.specimen.www.bean;
 
 
+import java.awt.image.BufferedImage;
+
 public class ImageWithSignPath {
     private int id;
+    private String svg;
+    private BufferedImage image;
+    private String hashCode;
+    private String sign;
     private String path;
 
     public ImageWithSignPath() {
     }
 
-    public ImageWithSignPath(int id, String path) {
+    public ImageWithSignPath(int id, String svg) {
         this.id = id;
+        this.svg = svg;
+    }
+
+    public ImageWithSignPath(int id, String svg, BufferedImage image, String hashCode, String sign, String path) {
+        this.id = id;
+        this.svg = svg;
+        this.image = image;
+        this.hashCode = hashCode;
+        this.sign = sign;
         this.path = path;
     }
 
@@ -32,6 +47,75 @@ public class ImageWithSignPath {
      * 获取
      * @return path
      */
+    public String getSvg() {
+        return svg;
+    }
+
+    /**
+     * 设置
+     * @param svg
+     */
+    public void setSvg(String svg) {
+        this.svg = svg;
+    }
+
+    public String toString() {
+        return "ImageWithSign{id = " + id + ", path = " + svg + "}";
+    }
+
+
+    /**
+     * 获取
+     * @return image
+     */
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    /**
+     * 设置
+     * @param image
+     */
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    /**
+     * 获取
+     * @return hashCode
+     */
+    public String getHashCode() {
+        return hashCode;
+    }
+
+    /**
+     * 设置
+     * @param hashCode
+     */
+    public void setHashCode(String hashCode) {
+        this.hashCode = hashCode;
+    }
+
+    /**
+     * 获取
+     * @return sign
+     */
+    public String getSign() {
+        return sign;
+    }
+
+    /**
+     * 设置
+     * @param sign
+     */
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    /**
+     * 获取
+     * @return path
+     */
     public String getPath() {
         return path;
     }
@@ -43,10 +127,4 @@ public class ImageWithSignPath {
     public void setPath(String path) {
         this.path = path;
     }
-
-    public String toString() {
-        return "ImageWithSign{id = " + id + ", path = " + path + "}";
-    }
-
-
 }
