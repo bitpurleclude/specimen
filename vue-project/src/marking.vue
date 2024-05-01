65ad28bd561d03a1727b4ae5416abe5be8c0f663
bitpurleclude <1551758442@qq.com>
2024-05-01 13:55:26 +0800

    feat: 新增id获取照片并且抽取重复代码

---
+++ vue-project/src/marking.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div>
+    <input type="file" @change="handleFileUpload">
+    <img v-if="imageUrl" :src="imageUrl" alt="Preview">
+    <div v-for="(svgPath, index) in svgPaths" :key="index" v-html="svgPath"></div>
+  </div>
+</template>
+
+<script setup>
+import Vue from 'vue'
+import VTooltip from 'v-tooltip'
+
+Vue.use(VTooltip)
+
+import { ref } from 'vue';
+
+const imageUrl = ref(null);
+const svgPaths = ref([]);
+
+const handleFileUpload = event => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = e => {
+      imageUrl.value = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+};
+const getSvgPaths = () =>{
+  fetch('/api/svg-paths')
+      .then(response => response.json())
+      .then(data => {
+        svgPaths.value = data;
+      });
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
